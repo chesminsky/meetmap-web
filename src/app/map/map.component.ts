1eebf9ea6366eef825990d1bc1746ef2c5402431
chesminsky <chesminsky@gmail.com>
2018-11-25 23:19:54 +0300

    get distance

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -119,4 +119,65 @@ export class MapComponent implements OnInit, OnDestroy {
       enableHighAccuracy: true
     });
   }
+
+  public getFrends() {
+    return Object.keys(this.markers);
+  }
+
+  public getDistance(name) {
+    return this.getDistanceFromLatLon(
+      this.markers[this.userName].getPosition().lat(),
+      this.markers[this.userName].getPosition().lng(),
+      this.markers[name].getPosition().lat(),
+      this.markers[name].getPosition().lng()
+    )
+  }
+
+
+  // -- geo utils --
+
+  private radians(n) {
+    return n * (Math.PI / 180);
+  }
+
+  private degrees(n) {
+    return n * (180 / Math.PI);
+  }
+
+  private getBearing(startLat, startLong, endLat, endLong) {
+    startLat = this.radians(startLat);
+    startLong = this.radians(startLong);
+    endLat = this.radians(endLat);
+    endLong = this.radians(endLong);
+
+    var dLong = endLong - startLong;
+
+    var dPhi = Math.log(Math.tan(endLat / 2.0 + Math.PI / 4.0) / Math.tan(startLat / 2.0 + Math.PI / 4.0));
+    if (Math.abs(dLong) > Math.PI) {
+      if (dLong > 0.0)
+        dLong = -(2.0 * Math.PI - dLong);
+      else
+        dLong = (2.0 * Math.PI + dLong);
+    }
+
+    return (this.degrees(Math.atan2(dLong, dPhi)) + 360.0) % 360.0;
+  }
+
+  private getDistanceFromLatLon(lat1, lon1, lat2, lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = this.deg2rad(lat2 - lat1);  // deg2rad below
+    var dLon = this.deg2rad(lon2 - lon1);
+    var a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
+      Math.sin(dLon / 2) * Math.sin(dLon / 2)
+      ;
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c; // Distance in km
+    return Math.round(1000 * d);
+  }
+
+  private deg2rad(deg) {
+    return deg * (Math.PI / 180)
+  }
 }
