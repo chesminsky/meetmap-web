fe7bbbb0d93273e2bf8483645d834f59438b7ea2
chesminsky <chesminsky@gmail.com>
2018-12-03 21:41:56 +0300

    back button

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { NotificationsService } from '../_common/services/notifications.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
@@ -9,12 +11,19 @@ import { NotificationsService } from '../_common/services/notifications.service'
 })
 export class NavbarComponent implements OnInit {
 
+  public needBackButton: boolean;
+
   constructor(
     private auth: AuthService,
-    private notifications: NotificationsService
+    private notifications: NotificationsService,
+    private router: Router,
+    private location: Location
   ) { }
 
   ngOnInit() {
+    this.router.events.subscribe((event) => {
+        this.needBackButton = this.location.path().includes('/map');
+    });
   }
 
   get isAuthorized() {
@@ -23,6 +32,10 @@ export class NavbarComponent implements OnInit {
 
   get notificationsLength() {
     return this.notifications.list.length;
+  }
+
+  back() {
+    this.location.back();
   }
 
 }
