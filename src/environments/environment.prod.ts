b4f377d556e1265b3a8c6f36ef07fbca356a55fb
chesminsky <chesminsky@gmail.com>
2018-12-09 15:24:51 +0300

    prod uri

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // baseUrl: 'http://94.19.241.42:3000/',
-  baseUrl: 'http://192.168.1.104:3000/'
+  baseUrl: 'http://94.19.241.42:3000/',
+  //baseUrl: 'http://192.168.1.104:3000/'
 };
